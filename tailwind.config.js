e9e5d42626bf005698cc5383a429e84b36017762
Ezecodes-Phantom <osakwechibueze@gmail.com>
2024-02-20 12:23:31 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['dist/*.{html, js}'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			fontFamily: {
+				Inter: 'Inter',
+			},
+			flexBasis: {
+				toTwo: '48%',
+				toThree: '31%',
+			},
+		},
+	},
+	plugins: [],
+};
